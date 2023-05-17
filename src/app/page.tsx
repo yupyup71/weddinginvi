0157e453f328f39eaba7887652fffaeca7274f9f
Yubeen <lybin71@gmail.com>
2023-05-17 01:25:46 +0100

    may17commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Coverphoto } from '@/components/coverphoto'
 import { Weddingdate } from '@/components/date'
+import { Secondtexts } from '@/components/secondtext'
 import { Texts } from '@/components/texts'
 import { useRouter } from 'next/router'
 
@@ -10,6 +11,13 @@ export default function Home() {
         <Weddingdate yearDate='2024年' monthDate='3月' dayDate='30日' />
         <Coverphoto coverPhoto='/KakaoTalk_2.jpg' />
         <Texts textyouWant="니콜라&이유빈" weddingtime="토요일 오후 3시" place="서울 어딘가에서"/>
+      </section>
+      <section style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+        <Secondtexts blah='서로가 마주보며 다져온 사랑을 이제 함께 한 곳을 바라보며 걸어갈 수 있는 큰 사랑으로 키우고자 합니다. 저희 두 사람이 사랑의 이름으로 지켜나갈 수 있도록 앞날을 축복해 주시면 감사하겠습니다.' 
+        nameus1='빈센트 & 안의 아들'
+        nameus2= '니콜라' 
+        nameus3= '이동기 & 남숙영의 딸'
+        nameus4= '이유빈'></Secondtexts>
       </section>
     </main>
   )
--- src/components/coverphoto.tsx
+++ src/components/coverphoto.tsx
@@ -3,7 +3,7 @@ export function Coverphoto(props: {
 }){
     let {coverPhoto} = props;
 
-    return <div style={{padding: "10px 0px"}} >
+    return <div style={{padding: "20px 0px 10px"}} >
          <img src={coverPhoto} style={{border:"0.5px solid grey", width:"400px"}}></img>
     </div>
 }
