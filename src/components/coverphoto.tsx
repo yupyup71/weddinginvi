ab4d5872614566a0a7f3fc1722a07f65d6c7dee6
Tormod Aase <tormod.aase@gmail.com>
2023-05-18 22:55:11 +0200

    chore: prettier

--- src/components/coverphoto.tsx
+++ src/components/coverphoto.tsx
@@ -1,9 +1,12 @@
-export function Coverphoto(props: {
-    coverPhoto: string; 
-}){
-    let {coverPhoto} = props;
+export function Coverphoto(props: { coverPhoto: string }) {
+  let { coverPhoto } = props;
 
-    return <div style={{padding: "20px 0px 10px"}} >
-         <img src={coverPhoto} style={{border:"0.5px solid grey", width:"400px"}}></img>
+  return (
+    <div style={{ padding: "20px 0px 10px" }}>
+      <img
+        src={coverPhoto}
+        style={{ border: "0.5px solid grey", width: "400px" }}
+      ></img>
     </div>
+  );
 }
